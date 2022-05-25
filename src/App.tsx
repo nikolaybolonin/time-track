53e7a8ec72ef0a9fcd1d61dc94805ec1589d7f21
Nikolay Bolonin <nibo@kvk.kraftvaerk.com>
2022-05-25 09:52:18 +0300

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+export const Body = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+function App() {
+  return (
+    <Body>
+      <header className="App-header">
+Timetracker
+      </header>
+    </Body>
+  );
+}
+
+export default App;
